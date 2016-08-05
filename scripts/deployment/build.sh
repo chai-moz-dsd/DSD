34195bb46060d9eedf51fd435179519a1a1d9c90
bfeng <fungbo@126.com>
2016-08-05 10:28:41 +0800

    [Deployment] - Add sudo for docker commands

--- scripts/deployment/build.sh
+++ scripts/deployment/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Copy Dockerfile to folder where the project root directory is
 cp ./chai/Dockerfile ./
 
-docker build -t chai/dsd .
+# Build docker image
+sudo docker build -t chai/dsd .
--- scripts/deployment/run.sh
+++ scripts/deployment/run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-docker rm -f dsd
+# Remove old docker container
+sudo docker rm -f dsd
 
-docker run -p 80:80 -p 8000:8000 --name=dsd -d chai/dsd
+# Run new docker container
+sudo docker run -p 80:80 -p 8000:8000 --name=dsd -d chai/dsd
