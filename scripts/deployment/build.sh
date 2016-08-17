a6b2df425305d63ad696018679fdf9e16248d02a
bfeng <fungbo@126.com>
2016-08-17 15:23:23 +0800

    [CI] - change the script build.sh, add tag when building docker image

--- scripts/deployment/build.sh
+++ scripts/deployment/build.sh
@@ -2,7 +2,7 @@
 
 # Copy Dockerfile to folder where the project root directory is
 echo "Copy Dockerfile"
-cp ./chai/Dockerfile ./
+cp ./${JOB_NAME}/Dockerfile ./
 
 echo "Build docker image"
-sudo docker build -t chai/dsd .
+sudo docker build -t chai/dsd:${BUILD_NUMBER} .
