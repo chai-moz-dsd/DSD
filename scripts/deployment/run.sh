cc00817cf6256409a919c40bcd058a0144d4a390
bfeng <fungbo@126.com>
2016-08-18 15:11:45 +0800

    [CI] - Change the remove docker container and run unit tests

--- scripts/deployment/run.sh
+++ scripts/deployment/run.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 echo "Remove old docker container"
-docker rm -f dsd
+docker ps -a | grep -P 'dsd$' | awk '{print$1}' | xargs -I {} docker rm {} || true
 
 echo "Run new docker container"
 docker run -p 80:80 -p 8000:8000 --name=dsd -d chai/dsd:${BUILD_NUMBER}
+
+echo "login container"
+docker exec -it dsd bash
+
+echo "run unit tests"
+source ./go ut
