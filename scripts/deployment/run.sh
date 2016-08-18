4bc12a0435d3e0847c08f306cfc5bba989d76750
bfeng <fungbo@126.com>
2016-08-18 15:18:23 +0800

    [CI] - Add force flag for docker rm command

--- scripts/deployment/run.sh
+++ scripts/deployment/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Remove old docker container"
-docker ps -a | grep -P 'dsd$' | awk '{print$1}' | xargs -I {} docker rm {} || true
+docker ps -a | grep -P 'dsd$' | awk '{print$1}' | xargs -I {} docker rm -f {} || true
 
 echo "Run new docker container"
 docker run -p 80:80 -p 8000:8000 --name=dsd -d chai/dsd:${BUILD_NUMBER}
