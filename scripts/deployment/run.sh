be91334d4c6a31d987c037e10bbc2b541b7b8d1d
bfeng <fungbo@126.com>
2016-08-18 15:35:29 +0800

    [CI] - Merge logining and runing unit tests into one line

--- scripts/deployment/run.sh
+++ scripts/deployment/run.sh
@@ -6,8 +6,5 @@ docker ps -a | grep -P 'dsd$' | awk '{print$1}' | xargs -I {} docker rm -f {} ||
 echo "Run new docker container"
 docker run -p 80:80 -p 8000:8000 --name=dsd -d chai/dsd:$1
 
-echo "login container"
-docker exec -it dsd bash
-
-echo "run unit tests"
-source ./go ut
+echo "login container and run unit tests"
+docker exec -it dsd bash ./go ut
