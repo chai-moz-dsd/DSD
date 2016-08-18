46c75ad3e8dba1d3802a4d278445ce4a1bf29aee
bfeng <fungbo@126.com>
2016-08-18 11:28:25 +0800

    [CI]- Remove sudo in run.sh

--- scripts/deployment/run.sh
+++ scripts/deployment/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Remove old docker container"
-sudo docker rm -f dsd
+docker rm -f dsd
 
 echo "Run new docker container"
-sudo docker run -p 80:80 -p 8000:8000 --name=dsd -d chai/dsd
+docker run -p 80:80 -p 8000:8000 --name=dsd -d chai/dsd:${BUILD_NUMBER}
