40c6b8332752782b52588324e2b4de21ad8c669c
bfeng <fungbo@126.com>
2016-08-22 10:40:28 +0800

    [#CI] - Tag the latest image

--- scripts/deployment/build.sh
+++ scripts/deployment/build.sh
@@ -11,4 +11,8 @@ echo "Remove old images"
 docker images | grep -P '^\S+dsd\s+([0-9]+)\b' | \
 awk 'BEGIN {BASELINE="'${BUILD_NUMBER}'"}{if($2 < BASELINE) print $1":"$2}' | xargs -I {} docker rmi -f {} || true
 
-docker images | grep "^<none>" | awk 'NR >=1 {print $3}' | xargs -I {} docker rmi -f {} || true
+echo "tag latest image"
+docker rmi chaimozdsd/dsd:latest
+docker tag chaimozdsd/dsd:${BUILD_NUMBER} chaimozdsd/dsd:latest
+
+docker images | grep "^<none>" | awk 'NR >= 1 {print $3}' | xargs -I {} docker rmi -f {} || true
