7594ba32a17e5b313a9ec308c83fb3c8d887d320
Shenjian YUAN <sjyuan@thoughtworks.com>
2016-08-22 16:20:50 +0800

    force remove running container

--- scripts/deployment/build.sh
+++ scripts/deployment/build.sh
@@ -14,7 +14,7 @@ docker images | grep -P '^\S+dsd\s+([0-9]+)\b' | \
 awk 'BEGIN {BASELINE="'${tag}'"}{if($2 < BASELINE) print $1":"$2}' | xargs -I {} docker rmi -f {} || true
 
 echo "tag latest image"
-docker rmi chaimozdsd/dsd:latest
+docker rmi -f chaimozdsd/dsd:latest
 docker tag chaimozdsd/dsd:${tag} chaimozdsd/dsd:latest
 
 docker images | grep "^<none>" | awk 'NR >= 1 {print $3}' | xargs -I {} docker rmi -f {} || true
