c44178256e6a4506161346376a90063f8dbc2715
Shenjian YUAN <sjyuan@thoughtworks.com>
2016-08-22 16:53:05 +0800

    update container name

--- scripts/deployment/run_ut.sh
+++ scripts/deployment/run_ut.sh
@@ -8,7 +8,7 @@ docker ps -a | grep -P 'dsd$' | awk '{print$1}' | xargs -I {} docker rm -f {} ||
 docker run -p 80:80 -p 8000:8000 --name=dsd-${tag} -d chaimozdsd/dsd:${tag}
 
 echo "login container and run unit tests"
-docker exec dsd bash ./go ut
+docker exec dsd-${tag} bash ./go ut
 
 echo "Remove old docker container"
 docker ps -a | grep -P 'dsd$' | awk '{print$1}' | xargs -I {} docker rm -f {} || true
