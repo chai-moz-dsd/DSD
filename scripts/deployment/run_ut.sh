079dd4d89d67d124dd2c6d5589a93813ded80dd8
Shenjian YUAN <sjyuan@thoughtworks.com>
2016-08-22 16:44:19 +0800

    fix ci

--- scripts/deployment/run_ut.sh
+++ scripts/deployment/run_ut.sh
@@ -4,11 +4,11 @@ tag=$1
 
 echo "Run new docker container"
 
-docker ps -a | grep -P 'dsd-${tag}' | awk '{print$1}' | xargs -I {} docker rm -f {} || true
+docker ps -a | grep -P 'dsd$' | awk '{print$1}' | xargs -I {} docker rm -f {} || true
 docker run -p 80:80 -p 8000:8000 --name=dsd-${tag} -d chaimozdsd/dsd:${tag}
 
 echo "login container and run unit tests"
 docker exec dsd bash ./go ut
 
 echo "Remove old docker container"
-docker ps -a | grep -P 'dsd-${tag}' | awk '{print$1}' | xargs -I {} docker rm -f {} || true
+docker ps -a | grep -P 'dsd$' | awk '{print$1}' | xargs -I {} docker rm -f {} || true
