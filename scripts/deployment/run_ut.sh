3d39ef0bc6241a7bf45fa48c94a525f9cfaa398a
Shenjian YUAN <sjyuan@thoughtworks.com>
2016-08-22 16:21:43 +0800

    test celery

--- scripts/deployment/run_ut.sh
+++ scripts/deployment/run_ut.sh
@@ -7,6 +7,7 @@ docker run -p 80:80 -p 8000:8000 --name=dsd -d chaimozdsd/dsd:${tag}
 
 echo "login container and run unit tests"
 docker exec dsd bash ./go ut
+docker exec dsd bash ./go sc
 
 echo "Remove old docker container"
 docker ps -a | grep -P 'dsd$' | awk '{print$1}' | xargs -I {} docker rm -f {} || true
