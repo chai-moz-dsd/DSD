91da3d1832c99598bd5183e52f77f731f6867447
Shenjian YUAN <sjyuan@thoughtworks.com>
2016-08-23 14:39:29 +0800

    test ci

--- scripts/deployment/run_ut.sh
+++ scripts/deployment/run_ut.sh
@@ -2,14 +2,16 @@
 
 tag=$1
 
-echo "Run new docker container"
+echo "Run new docker container build = ${tag}"
 docker run -p 80:80 -p 8000:8000 --name=dsd-${tag} -d chaimozdsd/dsd:${tag}
 
 echo "login container and run unit tests"
 docker exec dsd-${tag} bash ./go ut
-if [ $? -ne 0 ]; then
+OUT=$?
+echo ${OUT}
+if [ ${OUT} -ne 0 ]; then
     echo "Remove current container while the test is down."
-    docker ps -a | grep -P dsd-${tag} | awk '{print$1}' | xargs -I {} docker rm -f {} || true
+    docker rm -f dsd-${tag} || true
 fi
 
 echo "Remove current container"
