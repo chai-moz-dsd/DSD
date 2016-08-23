96629cb0da2eeb2ba91a5045fe99a615109eab59
Shenjian YUAN <sjyuan@thoughtworks.com>
2016-08-23 11:01:59 +0800

    remove unless line

--- scripts/deployment/run_ut.sh
+++ scripts/deployment/run_ut.sh
@@ -4,15 +4,13 @@ tag=$1
 
 echo "Run new docker container"
 docker run -p 80:80 -p 8000:8000 --name=dsd-${tag} -d chaimozdsd/dsd:${tag}
-if [ $? -ne 0 ]; then
-    docker ps -a | grep -P 'dsd-${tag}$' | awk '{print$1}' | xargs -I {} docker rm -f {} || true
-fi
 
 echo "login container and run unit tests"
 docker exec dsd-${tag} bash ./go ut
 if [ $? -ne 0 ]; then
+    echo "Remove current container while the test is down."
     docker ps -a | grep -P 'dsd-${tag}$' | awk '{print$1}' | xargs -I {} docker rm -f {} || true
 fi
 
-echo "Remove old docker container"
+echo "Remove current container"
 docker ps -a | grep -P 'dsd-${tag}$' | awk '{print$1}' | xargs -I {} docker rm -f {} || true
