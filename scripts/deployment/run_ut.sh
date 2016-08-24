d4ec13ea9238614150890b05398eed5114d93594
Shenjian YUAN <sjyuan@thoughtworks.com>
2016-08-24 11:27:32 +0800

    test for ci

--- scripts/deployment/run_ut.sh
+++ scripts/deployment/run_ut.sh
@@ -6,8 +6,7 @@ echo "Run new docker container build = ${tag}"
 docker run -p 80:80 -p 8000:8000 --name=dsd-${tag} -d chaimozdsd/dsd:${tag}
 
 echo "login container and run unit tests"
-docker exec dsd-${tag} bash ./go ut
-OUT=$?
+OUT=$(docker exec dsd-${tag} bash ./go ut)
 echo ${OUT}
 if [ ${OUT} -ne 0 ]; then
     echo "Remove current container while the test is down."
