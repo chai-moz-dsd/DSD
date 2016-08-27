722ee03b7f78a0c40e8474e5d92363ac2b25a719
Shenjian YUAN <sjyuan@thoughtworks.com>
2016-08-27 15:53:35 +0800

    [#CI] - successfully remove the container no matter whether the test is down

--- scripts/deployment/run_ft.sh
+++ scripts/deployment/run_ft.sh
@@ -7,9 +7,11 @@ docker run -p 80:80 -p 8000:8000 --name=dsd-${tag} -d chaimozdsd/dsd:${tag}
 
 echo "login container and run function tests"
 docker exec dsd-${tag} bash ./go ft
+
 if [ $? -ne 0 ]; then
     echo "Remove current container while the test is down."
     docker ps -a | grep -P dsd-${tag} | awk '{print$1}' | xargs -I {} docker rm -f {} || true
+    exit 1
 fi
 
 echo "Remove current container"
