ace716326f7d214dd63395c8373bedb286ec7b6d
Shenjian YUAN <sjyuan@thoughtworks.com>
2016-08-27 11:38:39 +0800

    [#CI] - test for ci

--- scripts/deployment/run_ut.sh
+++ scripts/deployment/run_ut.sh
@@ -8,5 +8,11 @@ docker run -p 80:80 -p 8000:8000 --name=dsd-${tag} -d chaimozdsd/dsd:${tag}
 echo "login container and run unit tests"
 docker exec dsd-${tag} bash ./go ut
 
+if [ $? -ne 0 ]; then
+    echo "Remove current container while the test is down."
+    docker ps -a | grep -P dsd-${tag} | awk '{print$1}' | xargs -I {} docker rm -f {} || true
+    exit 1
+fi
+
 echo "Remove current container"
 docker ps -a | grep -P dsd-${tag} | awk '{print$1}' | xargs -I {} docker rm -f {} || true
