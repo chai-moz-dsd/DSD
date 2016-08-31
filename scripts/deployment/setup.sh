a74cc960db5c3d2a1b140daea3d5e0147a43c8c0
bfeng <fungbo@126.com>
2016-08-31 16:02:37 +0800

    [#6] - Seed attributes and data elements in db

--- scripts/deployment/setup.sh
+++ scripts/deployment/setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 
-echo "migrate db"
+echo "setup db"
 source ~/.virtualenvs/dsd/bin/activate
 python manage.py migrate
+python manage.py loaddata dsd/fixtures/attributes.json
+python manage.py loaddata dsd/fixtures/dataElement.json
+
 
 echo "run cron job"
 python manage.py crontab add
