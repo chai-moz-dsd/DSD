a9678953465083b0f746a0dc9af799f0bbf831aa
bfeng <fungbo@126.com>
2016-09-01 11:05:36 +0800

    [#6] - Post organisation units and attributes to dhis2

---
+++ dsd/services/post_organ_and_attrs.py
@@ -0,0 +1,4 @@
+from dsd.services.dhis2_remote_service import post_organization_units, post_attributes, post_elements
+
+post_attributes()
+post_elements()
--- scripts/deployment/setup.sh
+++ scripts/deployment/setup.sh
@@ -7,6 +7,16 @@ python manage.py migrate
 python manage.py loaddata dsd/fixtures/attributes.json
 python manage.py loaddata dsd/fixtures/dataElement.json
 
+echo "post organisation units and attributes to dhis2"
+while true; do
+    sleep 1
+    if curl --fail dhis2; then
+        echo "posting."
+        python manage.py shell_plus < dsd.services.post_organ_and_attrs.py
+        break
+    fi;
+done
+
 
 echo "run cron job"
 python manage.py crontab add
