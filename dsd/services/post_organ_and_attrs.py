2c72aa8c688327ab050f51985b12a312c1a272ac
Jiayang Yan <raymond.dream@gmail.com>
2016-09-02 09:48:50 +0800

    [#6] - run create oauth when system startup

--- dsd/services/post_organ_and_attrs.py
+++ dsd/services/post_organ_and_attrs.py
@@ -1,4 +1,6 @@
-from dsd.services.dhis2_remote_service import post_organization_units, post_attributes, post_elements
+from dsd.repositories.dhis2_oauth_token import create_oauth
+from dsd.services.dhis2_remote_service import  post_attributes, post_elements
 
+create_oauth()
 post_attributes()
 post_elements()
--- dsd/test/helpers/fake_cache.py
+++
@@ -1,9 +0,0 @@
-class FakeCache():
-    @classmethod
-    def get(key):
-        if key == "refresh_token":
-            return "929a3cec-16b5-47bc-87c7-e90e6fbc8207"
-
-    @classmethod
-    def set(cls, key, value, expire_time):
-        return 0
