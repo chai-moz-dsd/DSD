d68216b43ae3426c8ca4dac4a24f37f350afed35
Jiayang Yan <raymond.dream@gmail.com>
2016-11-02 15:02:31 +0800

    [Script] add sync bussiness_data to local

--- dsd/services/post_metadata_service.py
+++ dsd/services/post_metadata_service.py
@@ -3,8 +3,7 @@ import logging
 from dsd.models import SyncRecord
 from dsd.scheduler import sync_business_data_to_local, sync_metadata_to_local
 from dsd.services.bes_middleware_core_service import fetch_updated_data_element_values
-from dsd.services.dhis2_remote_service import post_attributes, post_organization_units, post_category_options, post_categories, post_category_combinations, post_elements, post_data_set, assign_all_org_to_user, set_org_unit_level, \
-    post_data_element_values
+from dsd.services.dhis2_remote_service import post_attributes, post_organization_units, post_category_options, post_categories, post_category_combinations, post_elements, post_data_set, assign_all_org_to_user, set_org_unit_level,post_data_element_values
 from dsd.services.historical_data_service import post_historical_data_element_values_to_dhis2
 from dsd.services.sync_cocid_service import set_coc_id
 
@@ -34,9 +33,11 @@ def sync_metadata_with_dhis2():
     sync_bussiness_with_dhis2()
     post_historical_data_element_values_to_dhis2()
 
+
 def sync_bussiness_with_dhis2():
     date_element_values = fetch_updated_data_element_values()
     post_data_element_values(date_element_values)
+
 
 def need_sync_bes_data():
     return not SyncRecord.objects.filter(status='Success').count()
