9dd9fbf92a845e7f05d721994885c865ea4e5629
Shenjian YUAN <sjyuan@thoughtworks.com>
2016-11-17 10:04:59 +0800

    [#Deployment] - integrate validation rule addition to deployment

--- dsd/deployment/metadata_service.py
+++ dsd/deployment/metadata_service.py
@@ -1,9 +1,12 @@
 import logging
 
+from dsd.deployment.validation_initializer import post_all_validation_rule_and_group
 from dsd.models import SyncRecord
 from dsd.scheduler import sync_business_data_to_local, sync_metadata_to_local
 from dsd.services.bes_middleware_core_service import fetch_updated_data_element_values
-from dsd.services.dhis2_remote_service import post_attributes, post_organization_units, post_category_options, post_categories, post_category_combinations, post_elements, post_data_set, assign_all_org_to_user, set_org_unit_level,post_data_element_values
+from dsd.services.dhis2_remote_service import post_attributes, post_organization_units, post_category_options, \
+    post_categories, post_category_combinations, post_elements, post_data_set, assign_all_org_to_user, \
+    set_org_unit_level, post_data_element_values
 from dsd.services.historical_data_service import post_historical_data_element_values_to_dhis2
 from dsd.services.sync_cocid_service import set_coc_id
 
@@ -28,6 +31,9 @@ def sync_metadata_with_dhis2():
     post_elements()
     post_data_set()
     set_coc_id()
+
+    post_all_validation_rule_and_group()
+
     assign_all_org_to_user()
     set_org_unit_level()
     sync_business_with_dhis2()
@@ -46,5 +52,6 @@ def need_sync_bes_data():
 def sync_trigger():
     while need_sync_bes_data():
         sync_metadata_with_bes()
+
 
 sync_trigger()
