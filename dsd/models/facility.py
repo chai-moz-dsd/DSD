7698102c15878523df7b95e28edb94b9925490e3
bracken1983 <brackenbo@hotmail.com>
2017-03-06 22:03:24 +0800

    facility_name & sim_num & sim_num_opt & sim_serial & sim_serial_opt should not be unique.

--- dsd/models/facility.py
+++ dsd/models/facility.py
@@ -10,7 +10,7 @@ class Facility(TimeStampedModel):
         app_label = 'dsd'
 
     uid = models.CharField(max_length=255)
-    facility_name = models.CharField(max_length=255, unique=True)
+    facility_name = models.CharField(max_length=255)
     latitude = models.CharField(max_length=255, null=True)
     longitude = models.CharField(max_length=255, null=True)
     code_us = models.CharField(max_length=255, null=True)
@@ -18,15 +18,15 @@ class Facility(TimeStampedModel):
     level_us = models.CharField(max_length=255, null=True)
     fea_us = models.CharField(max_length=255, null=True)
     device_serial = models.CharField(max_length=255, null=True)
-    sim_number = models.CharField(max_length=255, unique=True, null=True)
-    sim_serial = models.CharField(max_length=255, unique=True, null=True)
+    sim_number = models.CharField(max_length=255, null=True)
+    sim_serial = models.CharField(max_length=255, null=True)
     device_number = models.CharField(max_length=255, null=True)
     state = models.IntegerField(null=True)
 
     person_contact_opt = models.CharField(max_length=255, null=True)
     phone_contact_opt = models.CharField(max_length=255, null=True)
-    sim_number_opt = models.CharField(max_length=255, unique=True, null=True)
-    sim_serial_opt = models.CharField(max_length=255, unique=True, null=True)
+    sim_number_opt = models.CharField(max_length=255, null=True)
+    sim_serial_opt = models.CharField(max_length=255, null=True)
     mac_number = models.CharField(max_length=255, null=True)
     device_serial_opt = models.CharField(max_length=255, null=True)
 
--- dsd/models/remote/facility.py
+++ dsd/models/remote/facility.py
@@ -9,7 +9,7 @@ class Facility(models.Model):
         app_label = 'chai'
         db_table = 'facilities'
 
-    facility_name = models.CharField(max_length=255, unique=True)
+    facility_name = models.CharField(max_length=255)
     latitude = models.CharField(max_length=255, null=True)
     longitude = models.CharField(max_length=255, null=True)
     code_us = models.CharField(max_length=255, null=True)
@@ -17,15 +17,15 @@ class Facility(models.Model):
     level_us = models.CharField(max_length=255, db_column='nivel_us', null=True)
     fea_us = models.CharField(max_length=255, db_column='fea_us', null=True)
     device_serial = models.CharField(max_length=255, null=True)
-    sim_number = models.CharField(max_length=255, unique=True, db_column='sim_numb', null=True)
-    sim_serial = models.CharField(max_length=255, unique=True, null=True)
+    sim_number = models.CharField(max_length=255, db_column='sim_numb', null=True)
+    sim_serial = models.CharField(max_length=255, null=True)
     device_number = models.CharField(max_length=255, db_column='device_numb', null=True)
     state = models.IntegerField(db_column='estado', null=True)
 
     person_contact_opt = models.CharField(max_length=255, db_column='pessoa_contacto_opt', null=True)
     phone_contact_opt = models.CharField(max_length=255, db_column='telefone_contacto_opt', null=True)
-    sim_number_opt = models.CharField(max_length=255, unique=True, db_column='sim_numb_opt', null=True)
-    sim_serial_opt = models.CharField(max_length=255, unique=True, null=True)
+    sim_number_opt = models.CharField(max_length=255, db_column='sim_numb_opt', null=True)
+    sim_serial_opt = models.CharField(max_length=255, null=True)
     mac_number = models.CharField(max_length=255, db_column='mac_numb', null=True)
     device_serial_opt = models.CharField(max_length=255, null=True)
 
