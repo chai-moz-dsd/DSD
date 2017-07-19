621410949a3abe57bd349692a00d25b717f782a6
bracken1983 <brackenbo@hotmail.com>
2017-07-19 13:24:18 +0800

    Add time interval before sync attributes.

--- dsd/services/dhis2_remote_service.py
+++ dsd/services/dhis2_remote_service.py
@@ -24,7 +24,7 @@ logger = logging.getLogger(__name__)
 
 
 def post_attributes():
-    time.sleep(10)
+    time.sleep(60)
     logger.info('=== START SYNC ATTRIBUTES ===')
     attributes = Attribute.objects.all()
     for attribute in attributes:
