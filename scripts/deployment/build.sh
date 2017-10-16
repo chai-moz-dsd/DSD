d6b4efb42c3287de7483146665f659239f383eb3
wywy08271 <wywy08271@outlook.com>
2017-10-16 20:26:01 +0800

    [Wang Yue] - modify docker configuration to build.sh

--- scripts/deployment/build.sh
+++ scripts/deployment/build.sh
@@ -7,14 +7,8 @@ cp ./${3}/Dockerfile ./
 echo "Build docker image"
 docker build -t chaimozdsd/dsd:${1} .
 
-echo "Remove old images"
-docker images | grep -P '^\S+dsd\s+([0-9]+)\b' | \
-awk 'BEGIN {BASELINE="'${1}'"}{if($2 < BASELINE) print $1":"$2}' | xargs -I {} docker rmi -f {} || true
-
-docker images | grep "^<none>" | awk 'NR >= 1 {print $3}' | xargs -I {} docker rmi -f {} || true
-
 password=${2}
 
 echo "push docker image to the repo"
-docker login -u="chaimozdsd" -p=${password}
+docker login -u="sakura164995249" -p=${password}
 docker push chaimozdsd/dsd:${1}
