5f6b9e417d661a7d32d981dcee9e9cb1d981b66c
wywy08271 <wywy08271@outlook.com>
2017-10-16 20:37:43 +0800

    Revert "[Wang Yue] - add build number to build.sh"
    This reverts commit 31c064172fff8e49d0f25c7d72bfeb7400dbf2d9.

--- scripts/deployment/build.sh
+++ scripts/deployment/build.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
+tag=${BUILD_NUMBER}
+
 # Copy Dockerfile to folder where the project root directory is
 echo "Copy Dockerfile"
-cp ./${3}/Dockerfile ./
+cp ./${JOB_NAME}/Dockerfile ./
 
 echo "Build docker image"
-docker build -t chaimozdsd/dsd:${1} .
+docker build -t chaimozdsd/dsd:${tag} .
 
 echo "Remove old images"
 docker images | grep -P '^\S+dsd\s+([0-9]+)\b' | \
-awk 'BEGIN {BASELINE="'${1}'"}{if($2 < BASELINE) print $1":"$2}' | xargs -I {} docker rmi -f {} || true
+awk 'BEGIN {BASELINE="'${tag}'"}{if($2 < BASELINE) print $1":"$2}' | xargs -I {} docker rmi -f {} || true
+
+echo "tag latest image"
+docker rmi -f chaimozdsd/dsd:latest || echo 'remove latest image failure'
+docker tag chaimozdsd/dsd:${tag} chaimozdsd/dsd:latest
 
 docker images | grep "^<none>" | awk 'NR >= 1 {print $3}' | xargs -I {} docker rmi -f {} || true
-
-password=${2}
-
-echo "push docker image to the repo"
-docker login -u="chaimozdsd" -p=${password}
-docker push chaimozdsd/dsd:${1}
